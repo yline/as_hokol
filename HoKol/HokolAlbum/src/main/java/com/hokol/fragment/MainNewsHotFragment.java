12dc68ad72fde54ae72cebbde336ca6afcd2e469
yline <957339173@qq.com>
2017-02-15 10:35:01 +0800

    this:广告位 next:瀑布流

--- HoKol/HokolAlbum/src/main/java/com/hokol/fragment/MainNewsHotFragment.java
+++ HoKol/HokolAlbum/src/main/java/com/hokol/fragment/MainNewsHotFragment.java
@@ -2,12 +2,18 @@ package com.hokol.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.hokol.R;
 import com.hokol.base.common.BaseFragment;
+import com.hokol.viewhelper.MainNewsHotADHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 热点
@@ -26,12 +32,22 @@ public class MainNewsHotFragment extends BaseFragment
 	public void onViewCreated(View view, @Nullable Bundle savedInstanceState)
 	{
 		super.onViewCreated(view, savedInstanceState);
-		initView();
+		initView(view);
 	}
 	
-	private void initView()
+	private void initView(View view)
 	{
-		NewsADFragment newsAdFragment = new NewsADFragment();
-		getChildFragmentManager().beginTransaction().add(R.id.ll_news_hot_ad, newsAdFragment).commit();
+		List<Integer> data = new ArrayList<>();
+		data.add(R.drawable.delete_ad_img1);
+		data.add(R.drawable.delete_ad_img2);
+		data.add(R.drawable.delete_ad_img3);
+		data.add(R.drawable.delete_ad_img4);
+		data.add(R.drawable.delete_ad_img5);
+
+		MainNewsHotADHelper mainNewsHotADHelper = new MainNewsHotADHelper();
+		mainNewsHotADHelper.build().setResource(data).commit(getContext());
+		mainNewsHotADHelper.initPoint((LinearLayout) view.findViewById(R.id.ll_main_hot_news_ad));
+		mainNewsHotADHelper.initViewPagerView((ViewPager) view.findViewById(R.id.viewpager_main_hot_news_ad));
+		mainNewsHotADHelper.startAutoRecycle();
 	}
 }
