3a7ba2ee906329078d29015fb7a737de0b9b55c1
yline <957339173@qq.com>
2017-03-16 21:10:31 +0800

    this:下拉二级over next:continue

--- HoKol/HokolAlbum/src/main/java/com/hokol/fragment/MainHomeFragment.java
+++ HoKol/HokolAlbum/src/main/java/com/hokol/fragment/MainHomeFragment.java
@@ -12,11 +12,16 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 import com.hokol.R;
+import com.hokol.application.IApplication;
 import com.hokol.base.common.BaseFragment;
+import com.hokol.medium.widget.SecondaryWidget;
 import com.hokol.viewhelper.MainHomeHelper;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainHomeFragment extends BaseFragment
 {
@@ -35,19 +40,48 @@ public class MainHomeFragment extends BaseFragment
 	{
 		mainHomeHelper = new MainHomeHelper(getContext());
 
+		initView(view);
+
+		// 下拉菜单栏
+		LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.ll_main_home_menu);
+		mainHomeHelper.initSecondaryView(new SecondaryWidget.OnSecondaryCallback()
+		{
+			@Override
+			public void onSecondarySelected(String first, String second)
+			{
+				IApplication.toast("first = " + first + ",second = " + second);
+				mainHomeHelper.closeMenu();
+			}
+		});
+		mainHomeHelper.initTabDownMenuView(linearLayout);
+
+		initData();
+		IApplication.getHandler().postDelayed(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				mainHomeHelper.setProvinceData(provinceMap);
+			}
+		}, 500);
+	}
+	
+	private void initView(View view)
+	{
+		fragmentList = new ArrayList<>();
+		fragmentList.add(new MainHomeRedFragment());
+		fragmentList.add(new MainHomeAuthorFragment());
+		fragmentList.add(new MainHomePerformerFragment());
+		fragmentList.add(new MainHomeModelFragment());
+		fragmentList.add(new MainHomeSingerFragment());
+		fragmentList.add(new MainHomeSportFragment());
+
 		// 主页 tab栏目
 		TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tab_layout_main_home);
 		ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewpager_main_home);
 		tabLayout.setupWithViewPager(viewPager);
 		tabLayout.setTabTextColors(getResources().getColor(android.R.color.black), getResources().getColor(android.R.color.holo_red_light));
 		tabLayout.setSelectedTabIndicatorColor(getResources().getColor(android.R.color.holo_red_light));
-
-		// 下拉菜单栏
-		LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.ll_main_home_menu);
-		mainHomeHelper.initTabDownMenuView(linearLayout);
-		mainHomeHelper.setProvinceData();
-
-		initView(view);
 
 		final String[] RES_MAIN_HOME_TAB = view.getResources().getStringArray(R.array.main_home_tab);
 		viewPager.setAdapter(new FragmentPagerAdapter(getChildFragmentManager())
@@ -71,15 +105,23 @@ public class MainHomeFragment extends BaseFragment
 			}
 		});
 	}
-	
-	private void initView(View view)
+
+	private Map<String, List<String>> provinceMap;
+
+	private void initData()
 	{
-		fragmentList = new ArrayList<>();
-		fragmentList.add(new MainHomeRedFragment());
-		fragmentList.add(new MainHomeAuthorFragment());
-		fragmentList.add(new MainHomePerformerFragment());
-		fragmentList.add(new MainHomeModelFragment());
-		fragmentList.add(new MainHomeSingerFragment());
-		fragmentList.add(new MainHomeSportFragment());
+		provinceMap = new HashMap<>();
+		provinceMap.put("北京市", Arrays.asList("北京"));
+		provinceMap.put("天津市", Arrays.asList("天津"));
+		provinceMap.put("黑龙江省", Arrays.asList("哈尔滨市", "齐齐哈尔市", "佳木斯市", "鹤岗市", "大庆市", "鸡西市", "双鸭山市", "伊春市", "牡丹江市", "黑河市", "七台河市", "绥化市和大兴安岭地区"));
+		provinceMap.put("河北省", Arrays.asList("石家庄", "唐山", "邯郸", "保定", "沧州", "邢台", "廊坊", "承德", "张家口", "衡水", "秦皇岛"));
+		provinceMap.put("山西省", Arrays.asList("大同", "朔州", "忻州", "太原", "阳泉", "晋中", "吕梁", "长治", "临汾", "晋城", "运城"));
+		provinceMap.put("内蒙古自治区", Arrays.asList("呼和浩特市", "包头市", "乌海市", "赤峰市", "通辽市", "鄂尔多斯市", "呼伦贝尔市", "巴彦淖尔市", "乌兰察布市"));
+		provinceMap.put("吉林省", Arrays.asList("长春市", "吉林市", "四平市", "辽源市", "通化市", "白山市", "白城市", "通化市", "松原市"));
+		provinceMap.put("江西省", Arrays.asList("南昌", "九江", "赣州", "吉安", "萍乡", "鹰潭", "新余", "宜春", "上饶", "景德镇", "抚州"));
+		provinceMap.put("海南省", Arrays.asList("海口市", "三亚市", "万宁市", "琼海市", "文昌市", "儋州市", "东方市", "五指山市．定安县", "乐东县", "澄迈县", "屯昌县", "临高县", "白沙黎族自治县"));
+		provinceMap.put("云南省", Arrays.asList("昆明市", "曲靖市", "玉溪市", "昭通市", "楚雄市", "普洱市", "景洪市", "大理市", "保山市", "丽江市", "临沧市", "宣威市", "个旧市", "文山市", "安宁市", "瑞丽市", "芒市"));
+		provinceMap.put("陕西省", Arrays.asList("铜川市", "宝鸡市", "咸阳市", "渭南市", "汉中市", "安康市", "商洛市", "延安市", "榆林市"));
+		provinceMap.put("青海省", Arrays.asList("格尔木市", "西宁市", "玉树", "果洛", "海东", "海西", "海南", "海北"));
 	}
 }
--- HoKol/HokolAlbum/src/main/java/com/hokol/viewhelper/MainHomeHelper.java
+++ HoKol/HokolAlbum/src/main/java/com/hokol/viewhelper/MainHomeHelper.java
@@ -3,21 +3,16 @@ package com.hokol.viewhelper;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import com.hokol.R;
-import com.hokol.base.adapter.CommonListAdapter;
-import com.hokol.base.adapter.ViewHolder;
 import com.hokol.medium.widget.DropMenuWidget;
 import com.hokol.medium.widget.SecondaryWidget;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class MainHomeHelper
 {
@@ -27,24 +22,19 @@ public class MainHomeHelper
 	{
 		this.context = context;
 	}
-	/* %%%%%%%%%%%%%%%%%%%%%%%%%%%%% TabLayout + ViewPager %%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
 	/* %%%%%%%%%%%%%%%%%%%%%%%%%%%%% 下拉菜单 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 	private String headers[] = {"城市", "筛选"};
 
-	private String provinceList[] = {"不限", "武汉", "北京", "上海", "成都", "广州", "深圳", "重庆", "天津", "西安", "南京", "杭州",
-			"武汉", "北京", "上海", "成都", "广州", "深圳", "重庆", "天津", "西安", "南京", "杭州",
-			"武汉", "北京", "上海", "成都", "广州", "深圳", "重庆", "天津", "西安", "南京", "杭州"};
-
 	private DropMenuWidget dropMenuWidget;
-
-	private ProvinceListAdapter provinceListAdapter;
 
 	private List<View> contentViewList = new ArrayList<>();
 
+	private SecondaryWidget secondaryWidget;
+
 	public void initSecondaryView(SecondaryWidget.OnSecondaryCallback listener)
 	{
-		SecondaryWidget secondaryWidget = new SecondaryWidget();
+		secondaryWidget = new SecondaryWidget();
 		View provinceView = secondaryWidget.start(context, listener);
 		contentViewList.add(provinceView);
 	}
@@ -53,9 +43,6 @@ public class MainHomeHelper
 	{
 		dropMenuWidget = new DropMenuWidget();
 
-		View provinceView = initAreaView();
-		contentViewList.add(provinceView);
-
 		View areaView = initFilterView();
 		contentViewList.add(areaView);
 
@@ -63,30 +50,14 @@ public class MainHomeHelper
 		linearLayout.addView(dropView);
 	}
 
-	public void setProvinceData()
+	public void setProvinceData(Map<String, List<String>> map)
 	{
-		provinceListAdapter.set(Arrays.asList(provinceList));
+		secondaryWidget.setDataMap(map);
 	}
 
-	private View initAreaView()
+	public void closeMenu()
 	{
-		View areaView = LayoutInflater.from(context).inflate(R.layout.fragment_main_home__menu_area, null);
-
-		final ListView provinceView = (ListView) areaView.findViewById(R.id.lv_main_home_menu_area_province);
-
-		provinceView.setDividerHeight(0);
-		provinceListAdapter = new ProvinceListAdapter(context);
-		provinceView.setAdapter(provinceListAdapter);
-		provinceView.setOnItemClickListener(new AdapterView.OnItemClickListener()
-		{
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-			{
-				provinceListAdapter.setCheckItem(position);
-			}
-		});
-
-		return areaView;
+		dropMenuWidget.closeMenu();
 	}
 
 	private View initFilterView()
@@ -94,47 +65,5 @@ public class MainHomeHelper
 		View filterView = LayoutInflater.from(context).inflate(R.layout.fragment_main_home__menu_filter, null);
 
 		return filterView;
-	}
-
-	private class ProvinceListAdapter extends CommonListAdapter<String>
-	{
-		private int checkItemPosition = 0;
-
-		public ProvinceListAdapter(Context context)
-		{
-			super(context);
-		}
-
-		public void setCheckItem(int position)
-		{
-			checkItemPosition = position;
-			notifyDataSetChanged();
-		}
-
-		@Override
-		protected int getItemRes(int i)
-		{
-			return android.R.layout.simple_list_item_1;
-		}
-
-		@Override
-		protected void setViewContent(int position, ViewGroup viewGroup, ViewHolder viewHolder)
-		{
-			TextView textView = viewHolder.get(android.R.id.text1);
-			textView.setText(sList.get(position));
-			if (checkItemPosition != -1)
-			{
-				if (checkItemPosition == position)
-				{
-					textView.setTextColor(sContext.getResources().getColor(android.R.color.holo_red_light));
-					textView.setCompoundDrawablesWithIntrinsicBounds(null, null, sContext.getResources().getDrawable(R.drawable.delete_item_menu_checked), null);
-				}
-				else
-				{
-					textView.setTextColor(sContext.getResources().getColor(android.R.color.black));
-					textView.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
-				}
-			}
-		}
 	}
 }
