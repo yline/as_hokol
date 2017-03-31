40954f756f35e3a7b3c9daea3c7e00daf3bf1af1
yline <957339173@qq.com>
2017-03-31 15:22:46 +0800

    this:关注界面 除了弹框 都解决了 next:主页

--- HoKol/HokolAlbum/src/main/java/com/hokol/activity/StarDynamicActivity.java
+++ HoKol/HokolAlbum/src/main/java/com/hokol/activity/StarDynamicActivity.java
@@ -3,8 +3,11 @@ package com.hokol.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.hokol.R;
+import com.hokol.application.IApplication;
+import com.hokol.base.adapter.ViewHolder;
 import com.hokol.base.common.BaseAppCompatActivity;
 
 /**
@@ -15,12 +18,49 @@ import com.hokol.base.common.BaseAppCompatActivity;
  */
 public class StarDynamicActivity extends BaseAppCompatActivity
 {
-	
+	private ViewHolder starDynamicViewHolder;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_star_dynamic);
+
+		starDynamicViewHolder = new ViewHolder(this.getWindow().getDecorView());
+		starDynamicViewHolder.get(R.id.tv_star_dynamic_care_or_cancel).setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				IApplication.toast("点击关注");
+			}
+		});
+		starDynamicViewHolder.get(R.id.tv_star_dynamic_give_gift).setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				IApplication.toast("点击送礼物");
+			}
+		});
+		starDynamicViewHolder.get(R.id.tv_star_dynamic_praise).setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				IApplication.toast("点击点赞");
+			}
+		});
+
+
+		starDynamicViewHolder.get(R.id.ll_star_dynamic_user).setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				StarInfoActivity.actionStart(StarDynamicActivity.this);
+			}
+		});
 	}
 
 	public static void actionStart(Context context)
