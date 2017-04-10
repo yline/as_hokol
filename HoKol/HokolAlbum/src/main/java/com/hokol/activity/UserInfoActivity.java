23b7e3085b566342e8fd04e919e374f3c78d9bb1
yline <957339173@qq.com>
2017-04-10 16:15:09 +0800

    this:我的资料 next:我的粉丝

--- HoKol/HokolAlbum/src/main/java/com/hokol/activity/UserInfoActivity.java
+++ HoKol/HokolAlbum/src/main/java/com/hokol/activity/UserInfoActivity.java
@@ -4,8 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.hokol.R;
+import com.hokol.medium.widget.LabelWidget;
+import com.hokol.medium.widget.labellayout.FlowLayout;
+
+import java.util.Arrays;
 
 public class UserInfoActivity extends AppCompatActivity
 {
@@ -15,6 +20,24 @@ public class UserInfoActivity extends AppCompatActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_user_info);
+
+		initView();
+	}
+
+	private void initView()
+	{
+		FlowLayout flowLayout = (FlowLayout) findViewById(R.id.flow_layout_user_info);
+		LabelWidget labelWidget = new LabelWidget(this, flowLayout);
+		labelWidget.setDataList(Arrays.asList("网红", "模特"));
+
+		findViewById(R.id.iv_user_info).setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				finish();
+			}
+		});
 	}
 	
 	public static void actionStart(Context context)
