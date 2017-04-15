70d6a0a4d76845cadccb291460fe9fa512c17228
yline <957339173@qq.com>
2017-04-15 16:45:09 +0800

    this:UI next:demo (浸入式 + 时间选择器 + okHttp缓存)

---
+++ HoKol/HokolAlbum/src/main/java/com/hokol/activity/UserInfoUpdateAreaActivity.java
@@ -0,0 +1,48 @@
+package com.hokol.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.hokol.R;
+import com.hokol.base.adapter.CommonRecyclerAdapter;
+import com.hokol.base.adapter.CommonRecyclerViewHolder;
+
+public class UserInfoUpdateAreaActivity extends AppCompatActivity
+{
+	private UserInfoAreaAdapter userInfoAreaAdapter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_user_info_update_area);
+
+		initView();
+	}
+
+	private void initView()
+	{
+		RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_user_info_update_area);
+		userInfoAreaAdapter = new UserInfoAreaAdapter();
+		recyclerView.setAdapter(userInfoAreaAdapter);
+		recyclerView.setLayoutManager(new LinearLayoutManager(this));
+	}
+
+	private class UserInfoAreaAdapter extends CommonRecyclerAdapter
+	{
+
+		@Override
+		public int getItemRes()
+		{
+			return 0;
+		}
+
+		@Override
+		public void setViewContent(CommonRecyclerViewHolder viewHolder, int position)
+		{
+
+		}
+	}
+}
