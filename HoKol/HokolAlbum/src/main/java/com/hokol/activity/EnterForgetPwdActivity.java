bd205901283d34728da9cca2ce7ca8c5a63b7c5d
yline <957339173@qq.com>
2017-05-15 15:35:33 +0800

    this:密码找回 界面over next:界面逻辑

--- HoKol/HokolAlbum/src/main/java/com/hokol/activity/EnterForgetPwdActivity.java
+++ HoKol/HokolAlbum/src/main/java/com/hokol/activity/EnterForgetPwdActivity.java
@@ -1,14 +1,10 @@
 package com.hokol.activity;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
-import android.view.View;
 
 import com.hokol.R;
-import com.hokol.application.IApplication;
 import com.yline.base.BaseAppCompatActivity;
 
 public class EnterForgetPwdActivity extends BaseAppCompatActivity
@@ -19,43 +15,8 @@ public class EnterForgetPwdActivity extends BaseAppCompatActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_enter_forget_pwd);
-
-		findViewById(R.id.tv_enter_forget_pwd_cancel).setOnClickListener(new View.OnClickListener()
-		{
-			@Override
-			public void onClick(View v)
-			{
-				finish();
-			}
-		});
-
-		findViewById(R.id.btn_enter_forget_pwd_commit).setOnClickListener(new View.OnClickListener()
-		{
-			@Override
-			public void onClick(View v)
-			{
-				showDialog();
-			}
-		});
 	}
-
-	private void showDialog()
-	{
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setTitle(R.string.enter_forget_pwd_dialog_hint);
-		builder.setPositiveButton(R.string.enter_forget_pwd_dialog_commit, new DialogInterface.OnClickListener()
-		{
-			@Override
-			public void onClick(DialogInterface dialog, int which)
-			{
-				IApplication.toast("ok");
-				EnterForgetPwdNewActivity.actionStart(EnterForgetPwdActivity.this);
-			}
-		});
-		AlertDialog alertDialog = builder.create();
-		alertDialog.show();
-	}
-
+	
 	public static void actionStart(Context context)
 	{
 		context.startActivity(new Intent(context, EnterForgetPwdActivity.class));
