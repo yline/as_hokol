65a4da26a96aa3792bb7bab721b80e3bebdd95ce
yline <957339173@qq.com>
2017-05-22 22:54:17 +0800

    this:发通告 界面 over next:完成 发任务界面填写信息的 作用

--- HoKol/HokolAlbum/src/main/java/com/hokol/activity/TaskPublishActivity.java
+++ HoKol/HokolAlbum/src/main/java/com/hokol/activity/TaskPublishActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import com.hokol.R;
 import com.yline.base.BaseAppCompatActivity;
+import com.yline.view.common.ViewHolder;
 
 /**
  * 发布任务
@@ -15,12 +16,22 @@ import com.yline.base.BaseAppCompatActivity;
  */
 public class TaskPublishActivity extends BaseAppCompatActivity
 {
-	
+	private ViewHolder viewHolder;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_task_publish);
+
+		viewHolder = new ViewHolder(this);
+
+		initView();
+	}
+
+	private void initView()
+	{
+
 	}
 
 	public static void actionStart(Context context)
