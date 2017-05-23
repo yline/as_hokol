3bdf0c2e29c24f00c0ac5222ac12585494c36603
yline <957339173@qq.com>
2017-05-23 09:44:55 +0800

    this:发布任务，UI大概一下，填写信息不完善

--- HoKol/HokolAlbum/src/main/java/com/hokol/activity/TaskPublishActivity.java
+++ HoKol/HokolAlbum/src/main/java/com/hokol/activity/TaskPublishActivity.java
@@ -3,8 +3,13 @@ package com.hokol.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.hokol.R;
+import com.yline.application.SDKManager;
 import com.yline.base.BaseAppCompatActivity;
 import com.yline.view.common.ViewHolder;
 
@@ -18,6 +23,10 @@ public class TaskPublishActivity extends BaseAppCompatActivity
 {
 	private ViewHolder viewHolder;
 
+	private DrawerLayout drawerLayout;
+
+	private RelativeLayout relativeLayout;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -27,11 +36,65 @@ public class TaskPublishActivity extends BaseAppCompatActivity
 		viewHolder = new ViewHolder(this);
 
 		initView();
+		initViewClick();
 	}
 
 	private void initView()
 	{
+		drawerLayout = viewHolder.get(R.id.drawer_task_publish);
+		drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED); // 禁止手势滑动
 
+		relativeLayout = (RelativeLayout) findViewById(R.id.rl_task_publish_right);
+	}
+
+	private void initViewClick()
+	{
+		viewHolder.setOnClickListener(R.id.iv_task_publish_back, new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				finish();
+			}
+		});
+
+		viewHolder.setOnClickListener(R.id.ll_task_publish_content_style, new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				SDKManager.toast("选择属性");
+
+				drawerLayout.openDrawer(Gravity.RIGHT);
+			}
+		});
+
+		viewHolder.setOnClickListener(R.id.ll_task_publish_content_area, new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				SDKManager.toast("选择地区");
+			}
+		});
+
+		viewHolder.setOnClickListener(R.id.ll_task_publish_content_time, new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				SDKManager.toast("选择时间");
+			}
+		});
+
+		viewHolder.setOnClickListener(R.id.btn_task_publish_content_commit, new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				SDKManager.toast("提交任务");
+			}
+		});
 	}
 
 	public static void actionStart(Context context)
