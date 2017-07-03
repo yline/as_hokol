db32b7914aa162e8124ff86d02138ad4a1cedf56
yline <957339173@qq.com>
2017-07-04 07:05:38 +0800

    this:我的账户 开始写 next:其他两个tab下的 fragment貌似也要调整

--- HoKol/HokolAlbum/src/main/java/com/hokol/fragment/UserAccountProfitFragment.java
+++ HoKol/HokolAlbum/src/main/java/com/hokol/fragment/UserAccountProfitFragment.java
@@ -1,18 +1,46 @@
 package com.hokol.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.hokol.R;
 import com.yline.base.BaseFragment;
+import com.yline.view.recycler.holder.ViewHolder;
 
 public class UserAccountProfitFragment extends BaseFragment
 {
+	private ViewHolder viewHolder;
+
 	public static UserAccountProfitFragment newInstance()
 	{
-		
 		Bundle args = new Bundle();
 		
 		UserAccountProfitFragment fragment = new UserAccountProfitFragment();
 		fragment.setArguments(args);
 		return fragment;
+	}
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+	{
+		return inflater.inflate(R.layout.fragment_user_account_profit, container, false);
+	}
+
+	@Override
+	public void onViewCreated(View view, @Nullable Bundle savedInstanceState)
+	{
+		super.onViewCreated(view, savedInstanceState);
+
+		viewHolder = new ViewHolder(view);
+		initView();
+	}
+
+	private void initView()
+	{
+
 	}
 }
