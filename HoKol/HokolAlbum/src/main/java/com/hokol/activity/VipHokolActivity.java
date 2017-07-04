47552a6cb81ffd4e13b7036360cc501dcfa53186
yline <957339173@qq.com>
2017-07-04 16:14:48 +0800

    this:我的会员 界面ok next:会员升级 界面

--- HoKol/HokolAlbum/src/main/java/com/hokol/activity/VipHokolActivity.java
+++ HoKol/HokolAlbum/src/main/java/com/hokol/activity/VipHokolActivity.java
@@ -3,13 +3,17 @@ package com.hokol.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.hokol.R;
 import com.yline.base.BaseAppCompatActivity;
+import com.yline.view.recycler.holder.ViewHolder;
 
 public class VipHokolActivity extends BaseAppCompatActivity
 {
-	
+
+	private ViewHolder viewHolder;
+
 	public static void actionStart(Context context)
 	{
 		context.startActivity(new Intent(context, VipHokolActivity.class));
@@ -20,5 +24,19 @@ public class VipHokolActivity extends BaseAppCompatActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_vip_hokol);
+
+		viewHolder = new ViewHolder(this);
+	}
+
+	private void initView()
+	{
+		viewHolder.setOnClickListener(R.id.iv_vip_singlle_hokol_cancel, new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				finish();
+			}
+		});
 	}
 }
--- HoKol/HokolAlbum/src/main/java/com/hokol/activity/VipSinglePrivateActivity.java
+++ HoKol/HokolAlbum/src/main/java/com/hokol/activity/VipSinglePrivateActivity.java
@@ -3,13 +3,16 @@ package com.hokol.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.hokol.R;
 import com.yline.base.BaseAppCompatActivity;
-import com.yline.utils.UIScreenUtil;
+import com.yline.view.recycler.holder.ViewHolder;
 
 public class VipSinglePrivateActivity extends BaseAppCompatActivity
 {
+	private ViewHolder viewHolder;
+
 	public static void actionStart(Context context)
 	{
 		context.startActivity(new Intent(context, VipSinglePrivateActivity.class));
@@ -21,9 +24,19 @@ public class VipSinglePrivateActivity extends BaseAppCompatActivity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_vip_single_private);
 
-		int width = UIScreenUtil.getScreenWidth(this);
-		// UIResizeUtil.build()
-		// RadioButton radioButton;
-		// radioButton.setForeground();
+		viewHolder = new ViewHolder(this);
+		initView();
+	}
+
+	private void initView()
+	{
+		viewHolder.setOnClickListener(R.id.iv_vip_singlle_private_cancel, new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				finish();
+			}
+		});
 	}
 }
